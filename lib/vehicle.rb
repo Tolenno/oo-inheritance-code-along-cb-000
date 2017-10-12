b8e45a39c66fa58fca38aa6063ccefc5e3dee566
Matt Hough <matt@hough.tv>
2017-10-12 20:50:45 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,10 @@
 class Vehicle
-
+  attr_reader :wheel, :size
+  attr_accessor :number
+  
+  def initialize(wheel, size, number)
+    @wheel = wheel
+    @size = size
+    @number = number
+  end
 end
